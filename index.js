55ba3128cbbc3c6c6a73ece51b92925af7ea9417
nbx7 <101077340+nbx7@users.noreply.github.com>
2022-12-13 11:37:52 -0300

    maintenance

---
+++ index.js
@@ -0,0 +1,39 @@
+const pup = require('puppeteer');
+const readLineSync = require('readline-sync');
+const fs = require('fs');
+console.log('Bem vindo ao seu botzinho para encontrar letras de musicas!');
+const url = 'https://genius.com/';
+
+async function bot() {
+    const searchFor = readLineSync.question('digite o nome do artista seguido da musica que deseja pesquisar (evite usar caracteres especiais): ');
+    const browser = await pup.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    console.log('buscando...')
+    await page.goto(url);
+    await page.waitForSelector('input[name="q"]');
+    await page.type('input[name="q"]', searchFor);
+    await page.click('.PageHeaderSearchdesktop__Icon-eom9vk-1');
+    await page.waitForNavigation();
+    await page.click('.u-clickable .mini_card');
+    console.log('prontinho!');
+    const result = await page.$$eval('div[data-lyrics-container="true"]', el => el.map(lt => lt.innerText));
+    const resultFormated = result.toString().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+    console.log(resultFormated);
+    console.log('');
+    console.log('');
+    const saved = readLineSync.question('Deseja salvar o arquivo (yes/no)? ');
+    if (saved == 'yes') {
+        fs.writeFile(`./lyrics_sources/${searchFor}.txt`, resultFormated, err => {
+            if (err) throw new Error('error! cannot be saved or found.');
+            return console.log('Arquivo(s) salvo!');
+        });
+    };
+    const newSearch = readLineSync.question('Deseja fazer uma nova pesquisa (yes/no)? ');
+    if (newSearch == 'yes') {
+        bot();
+    };
+    await browser.close();
+};
+
+bot();
